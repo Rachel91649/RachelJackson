737c35aa884c86e67485caed5044c512a546883c
Rachel Jackson <racheljackson@Rachels-MacBook-Pro-2.local>
2016-07-18 09:36:50 -0400

    updating styling and links

---
+++ main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+	console.log("my portfolio");
+
+
+	//grab containers by id
+	//about page
+		//on click of about in nav bar
+			//hide all contents of the page
+			//unhide the about me contents
+	//contact page
+		//on click of the contact me in nav bar
+			//hide all contents of the page
+			//unhide the contact information		
+	//hit projects
+	//on click of code (in nav bar) unhide and append vertically on right side of pageprojects
+
+});
